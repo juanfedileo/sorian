dbea125bf2c1e212cd17538c2753119671fd247e
agust <agust@LAPTOP-ACCOT16F>
2021-05-08 02:39:00 -0300

    teesssst

---
+++ AnalisisYDiseño3/src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+import Vista.*;
+import controlador.*;
+import Modelo.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VentanaCliente ClienteVista;
+		ClienteVista = new VentanaCliente();
+		ClienteVista.setVisible(true);
+	}
+
+}
